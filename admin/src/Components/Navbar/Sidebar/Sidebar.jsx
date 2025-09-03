e46d9b6f7921670ad6c70a4e73d8643b117dac61
Kerbay23 <kerbaysteven@gmail.com>
2025-09-03 20:21:36 +0200

    second commit

--- admin/src/Components/Navbar/Sidebar/Sidebar.jsx
+++ admin/src/Components/Navbar/Sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import product_icon from '../../assets/'
 const Sidebar = () => {
   return (
     <div className="didebar">
-        <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+        <Link to={'/addproduct'} style={{textDecoration:"none"}}>.
         <div className="sidebar-item">
          <img src={product_icon} alt="" />
         </div>
